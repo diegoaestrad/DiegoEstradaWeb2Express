0ed37896f5c708da0a38e209ac9a21f83bbb6ae2
Diego Estrada Beltran <86diegoestrada@gmail.com>
2020-11-11 09:19:26 +0000

    Adding the hbs files with the message to be showed using also the parameters inside the res.render method inside the router.get

--- routes/index.js
+++ routes/index.js
@@ -8,22 +8,22 @@ router.get('/', function(req, res, next) {
 
 /* GET About page. */
 router.get('/about', (req, res, next) => {
-    res.render('about', { message: 'Diego Estrada - 200427046', url: 'https://ca.linkedin.com/in/ingenierodesistemas'})
+    res.render('about', { message: 'Diego Estrada - 200427046', window: 'About Window', url: 'https://ca.linkedin.com/in/ingenierodesistemas'})
 });
 
 /* GET Projects page. */
 router.get('/projects', (req, res, next) => {
-    res.render('projects', { message: 'Diego Estrada - 200427046'})
+    res.render('projects', { message: 'Diego Estrada - 200427046', window: 'Project Window'})
 });
 
 /* GET Services page. */
 router.get('/services', (req, res, next) => {
-    res.render('services', { message: 'Diego Estrada - 200427046' })
+    res.render('services', { message: 'Diego Estrada - 200427046', window: 'Services Window'})
 });
 
 /* GET Contact page. */
 router.get('/contact', (req, res, next) => {
-    res.render('contact', { message: 'Diego Estrada - 200427046', url: 'https://ca.linkedin.com/in/ingenierodesistemas' })
+    res.render('contact', { message: 'Diego Estrada - 200427046', window: 'Contact Window' })
 });
 
 module.exports = router;
